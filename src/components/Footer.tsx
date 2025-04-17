3d29503eb16c174ebe6e636029ff7891d1cf02cf
Alen Koikkara <alendennis77@gmail.com>
2025-04-17 17:01:40 -0500

    footer links added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,9 +4,13 @@ const Footer: React.FC = () => {
   return (
     <div className="flex justify-between items-center gap-10 absolute bottom-5 right-5 text-black dark:text-white text-center">
       <div className="flex text-md gap-2">
-        <div className="cursor-pointer hover:text-linkedin transition-all duration-300">LI</div>
+        <div className="cursor-pointer hover:text-linkedin transition-all duration-300">
+          <a href="https://www.linkedin.com/in/alenkoikkara/" target="_blank" rel="noopener noreferrer">LI</a>
+        </div>
         <div>|</div>
-        <div className="cursor-pointer hover:text-github transition-all duration-300">GH</div>
+        <div className="cursor-pointer hover:text-github transition-all duration-300">
+          <a href="https://github.com/alenkoikkara" target="_blank" rel="noopener noreferrer">GH</a>
+        </div>
       </div>
       <div className="text-xs">© 2025 Alen Koikkara</div>
     </div>
