5dfd5b09b4dda6809b12cc25d8bec278ae8109d2
Alen Koikkara <alendennis77@gmail.com>
2025-04-16 23:36:25 -0500

    layout fix 4

--- src/components/LandingCard.tsx
+++ src/components/LandingCard.tsx
@@ -3,8 +3,8 @@ import { motion } from 'framer-motion'
 const LandingCard: React.FC = () => {
   return (
     <div className="flex py-[30px] flex-col justify-center items-start gap-[10%] text-4xl h-full w-full font-bold bg-image bg-blackboard-black dark:bg-white">
-        <div className="w-full flex flex-col justify-center items-start gap-[16px] text-white dark:text-blackboard-black text-[clamp(2.2rem,11vw,20rem)] md:text-[clamp(2rem,6vw,7rem)] font-[300] transition-all duration-300 relative">
-          <div className="px-[30px] leading-[1] flex gap-4 flex-wrap">
+        <div className="w-full flex flex-col justify-center items-start gap-2 md:gap-4 text-white dark:text-blackboard-black text-[clamp(2.3rem,11.5vw,20rem)] md:text-[clamp(2rem,6vw,7rem)] font-[300] transition-all duration-300 relative">
+          <div className="px-[30px] leading-[1] flex gap-2 md:gap-4 flex-wrap">
             <motion.span
               initial={{ opacity: 0, y: 20, filter: "blur(10px)" }}
               animate={{ opacity: 1, y: 0, filter: "blur(0px)" }}
@@ -25,18 +25,11 @@ const LandingCard: React.FC = () => {
               transition={{ duration: .8, delay: 0.4 }}
               className="text-sun font-['Montaga']"
             >
-              change
-            </motion.span>
-            <motion.span
-              initial={{ opacity: 0, y: 20, filter: "blur(10px)" }}
-              animate={{ opacity: 1, y: 0, filter: "blur(0px)" }}
-              transition={{ duration: .8, delay: 0.6 }}
-            >
-              ,
+              change,
             </motion.span>
           </div>
           <div className="h-[.5px] mt-2 w-full bg-slate opacity-50 hidden md:block"></div>
-          <div className="px-[30px] leading-[1] flex gap-4 flex-wrap">
+          <div className="px-[30px] leading-[1] flex gap-2 md:gap-4 flex-wrap">
             <motion.span
               initial={{ opacity: 0, y: 20, filter: "blur(10px)" }}
               animate={{ opacity: 1, y: 0, filter: "blur(0px)" }}
