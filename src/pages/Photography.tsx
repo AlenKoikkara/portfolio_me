91114acb71ffe41dfb71b300462ca126731f0745
alenkoikkara <alendennis77@gmail.com>
2025-05-17 15:48:59 -0500

    fix to build

--- src/pages/Photography.tsx
+++ src/pages/Photography.tsx
@@ -1,36 +1,6 @@
 import React from 'react';
 import PhotographyCarousel from '../components/PhotographyCarousel';
 import WebLayout from '../layout/photographylayout/WebLayout';
-import agletmain from "../assets/images/agletmain.png";
-
-// Sample photos - replace with your actual photos
-const photos = [
-  {
-    id: 1,
-    imageUrl: agletmain,
-    subtitle: 'Sunset at the Beach'
-  },
-  {
-    id: 2,
-    imageUrl: agletmain,
-    subtitle: 'Mountain Landscape'
-  },
-  {
-    id: 3,
-    imageUrl: agletmain,
-    subtitle: 'Urban Architecture'
-  },
-  {
-    id: 4,
-    imageUrl: agletmain,
-    subtitle: 'Nature Close-up'
-  },
-  {
-    id: 5,
-    imageUrl: agletmain,
-    subtitle: 'Street Photography'
-  }
-];
 
 const Photography: React.FC = () => {
   return (
@@ -51,7 +21,7 @@ const Photography: React.FC = () => {
       <WebLayout>
         <div className="flex flex-col h-full overflow-hidden">
           <div className="h-screen w-full overflow-hidden text-white">
-            <PhotographyCarousel photos={photos} />
+            <PhotographyCarousel />
           </div>
         </div>
       </WebLayout>
